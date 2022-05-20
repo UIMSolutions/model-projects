243258d950eba4931e6bccf34242bfedfe156653
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-05-20 12:56:31 +0200

    Correct namespaces

---
+++ source/models/projects/entities/allocationkeys/package.d
@@ -0,0 +1,4 @@
+module models.projects.entities.allocationkeys;
+
+public import models.projects.entities.allocationkeys.allocationkey;
+public import models.projects.entities.allocationkeys.detail;
--- source/models/projects/entities/vendors/package.d
+++ source/models/projects/entities/vendors/package.d
@@ -1,5 +1,5 @@
 module models.projects.entities.vendors;
 
 // Entities
-public import module models.projects.entities.vendors.retentionterm;
-public import module models.projects.entities.vendors.retentiontermschedule;
+public import models.projects.entities.vendors.retentionterm;
+public import models.projects.entities.vendors.retentiontermschedule;
